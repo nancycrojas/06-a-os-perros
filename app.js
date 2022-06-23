ffb4c1215fce5d10414c024b6813365e495f4b6c
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 08:56:08 -0300

    Variables, datos y operadores - ejercicio 6

---
+++ app.js
@@ -0,0 +1,8 @@
+//Años perro
+
+//Crear un programa que pida ingresar una edad y devuelva el equivalente de esas edad en años perro.
+
+
+const edadPerro = prompt ("Por favor ingresa la edad de tu perro");
+alert("La edad de tu perro es: " + edadPerro * 7)
+
