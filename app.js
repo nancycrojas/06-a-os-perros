ae4490859a697263c0bc6f810a09a09c1b6d4442
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 16:30:51 -0300

    Agregando template strings

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 //Años perro
 
-//Crear un programa que pida ingresar una edad y devuelva el equivalente de esas edad en años perro.
+//Crear un programa que pida ingresar una edad y devuelva el equivalente de esa edad en años perro.
 
 
-const edadPerro = prompt ("Por favor ingresa una edad");
-alert("La edad que ingresaste es: " + edadPerro * 7 + " años en años perro.")
+let edadPerro = Number(prompt("Por favor ingresa una edad"));
+//alert("La edad que ingresaste es: " + edadPerro * 7 + " años en años perro.")
+
+//Template strings
+alert(`La edad que ingresaste es: ${edadPerro * 7} años en años perro.`)
+
 
